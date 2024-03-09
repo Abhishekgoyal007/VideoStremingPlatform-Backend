a4efaf5bf617e2b0a61392434601615b3d478140
Abhishek goyal <abhishekgoyal1311@gmail.com>
2024-03-09 17:57:14 +0530

    Added user and video model also install jwt and bcrypt

---
+++ src/models/video.model.js
@@ -0,0 +1,47 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+    {
+        videoFile: {
+            type: String, // cloudniary : as we are going to save url in the database and this we will get from cloudinary at free
+            required: [true, "Video is required"]
+        },
+        thumbnail: {
+            type: String,
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        duration: {
+            type: Number,
+            required: true
+        },
+        views: {
+            type: Number,
+            default: 0,
+        },
+        isPublished: {
+            type: Boolen,
+            required: true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+videoSchema.plugin(mongooseAggregatePaginate)
+    
+export const Video = mongoose.model("Video",videoSchema)
